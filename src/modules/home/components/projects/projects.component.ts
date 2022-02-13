1ee938401c16ecaabd68ae7844c1d634184f9e80
pboucher44 <boucher.pierre44400@hotmail.fr>
2022-02-13 10:03:50 +0100

    Making sticky animation for projects

--- src/modules/home/components/projects/projects.component.ts
+++ src/modules/home/components/projects/projects.component.ts
@@ -11,6 +11,57 @@ export class ProjectsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.init();
   }
+
+
+  init(){
+    this.setStickyContainersSize();
+    this.bindEvents();
+  }
+
+  bindEvents(){
+    window.addEventListener("wheel", (evt) => this.wheelHandler(evt));
+    window.addEventListener("resize", () => this.setStickyContainersSize());
+  }
+
+  setStickyContainersSize(){
+    document.querySelectorAll('.sticky-container').forEach(function(container){
+      const mainSelector = container.querySelector('main')
+      if (mainSelector) {
+        const stikyContainerHeight = ((mainSelector.offsetWidth + window.innerHeight) * 2) + mainSelector.offsetWidth;
+        container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
+      }
+    });
+  }
+
+  isElementInViewport (el: HTMLElement) {
+    const rect = el.getBoundingClientRect();
+    return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
+  }
+
+  wheelHandler(evt: WheelEvent){
+
+    const containerInViewPort = Array.from(document.querySelectorAll('.sticky-container'))
+      .filter((container) =>{
+        return this.isElementInViewport(container as HTMLElement);
+      })[0];
+
+    if(!containerInViewPort || !(containerInViewPort instanceof HTMLElement)){
+      return;
+    }
+
+    var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
+    var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
+    let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
+
+    if(g_canScrollHorizontally){
+      const viewPortContainer = containerInViewPort.querySelector('main');
+      if (viewPortContainer) {
+        viewPortContainer.scrollLeft += evt.deltaY;
+      }
+    }
+  }
+
 
 }
